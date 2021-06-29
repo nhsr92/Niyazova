7feec2f3983d38edad9a1a83942900438ae0fa7f
rsh <marselniva6@gmail.com>
2021-06-29 23:55:12 +0300

    Задача 1.урок1

---
+++ lesson1/task1.py
@@ -0,0 +1,22 @@
+# int
+a = -20
+b = 20
+c = a + b
+print(a, b, c)
+
+a = 6467325236
+b = 123
+c = a/b
+print(type(c))
+
+# floating
+a = 1.2
+b = 3.4
+c = a - b
+print(c)
+
+# get number
+a = input('Введите число')
+b = input('Еще')
+print('Введите число','Еще')
+

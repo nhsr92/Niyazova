facfe0dfbb339bb12b3e39113817f91211cce65b
rsh <marselniva6@gmail.com>
2021-07-08 19:55:05 +0300

    Второй урок

---
+++ lesson2/unit1.py
@@ -0,0 +1,6 @@
+my_list = [(-1 + 0j), 1, 2.2, True, None, 'String', [3, 4],
+           (5, 6, 6.5), {7: 'seven', 8: 'eight'}, {9, 10},
+           frozenset(), range(11), b'twelve', bytearray(b'thirteen'),
+           zip(*[(14, 15), (16, 17), ('a', 'b')]), TypeError]
+# for i, item in enumerate(my_list, l):
+# print(f"{i}) {item} - {type(item)}")
